85d1e215f8f47cf6449603c6ed07e1f10b9c04a5
falsethirteenth <ethgdfhbacii@gmail.com>
2022-09-15 14:32:06 +0300

    Added new files

---
+++ lib/module/inc-footer.php
@@ -0,0 +1,147 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div>
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Главная</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/blog">
+										<span>Блог</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="about">
+										<span>Истории</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/compatibility">
+										<span>Факты</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/download">
+										<span>PacMan</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/quickstart">
+										<span>Анекдоты</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="/branding">
+										<span>Spotify</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Ивенты</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://github.com/RPCS3/rpcs3" target="_blank">
+										<span>PacMAN</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Соц. сети</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.instagram.com/false.13/" target="_blank">
+										<span>Instagram</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/channel/UCPytJIQzbYnITvLtGc0LNFw" target="_blank">
+										<span>YouTube</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.tiktok.com/@false_react/" target="_blank">
+										<span>TikTok</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.reddit.com/user/false_thirteenth" target="_blank">
+										<span>Reddit</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td class="footer-con-sources">
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Поддержка</span>
+									</div>
+								     <div class="footer-tx2-bound">
+										<a href="https://send.monobank.ua/jar/7mRyMTN3Gu" target="_blank">
+										<span>Monobank</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<div class="footer-ico-developer" style="background: url('/img/icons/footer/13.jpg') center center / cover no-repeat;">
+								</div>
+								<span>Author: FALSE THIRTEENTH</span>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<div class="footer-ico-developer" style="background: url('/img/icons/footer/7.jpg') center / cover no-repeat;">
+								</div>
+								<span>beta-version © 2022</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
---
+++ lib/module/inc-video-carousel.php
@@ -0,0 +1,87 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/1123.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Darling in The Franxx「AMV」- tell me why i'm waiting</h2>
+								<h3>В мире будущего у детей, что растут без родителей в изолированных Плантациях, нет другой роли кроме как управлять гигантскими машинами – Франксами – и с их помощью сражаться с Кричащими драконами – чудовищами, угрожающими остаткам человечества, живущим под землёй. Несмотря на то, что детям вместо имён дают номера, один мальчик решает назваться Хиро и придумывает имена своим друзьям. И когда Хиро уже готовится пилотировать Франкс, он проваливает тест. Положение спасает загадочная девушка с рожками, чей код – 02.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-2'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/1500.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>「AMV」lil peep x lil tracy - witchblades</h2>
+								<h3>Nothing could be worse than losing someone close to you.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/3.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>ТОП ЛУЧШИХ АНИМЕ В ЖАНРЕ РОМАНТИКА</h2>
+								<h3>✨Лучшие романтические аниме всех времен.✨</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
